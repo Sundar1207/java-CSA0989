641ebe3de17e58be5a1371e83e72f4c79a8cd6bd
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-10-08 00:13:25 +0530

    Create Functionalinterface_2.java

---
+++ Functional InterFace and labda Funtions/Functionalinterface_2.java
@@ -0,0 +1,27 @@
+// it is return type labda expresstion
+
+interface a 
+{
+    static void main(int a , int b)
+    {
+        System.out.println("Sum of two numbers : "+(a+b));
+        rule2();
+    }
+    private static void rule2()
+    {
+        System.out.println("Hello java ");
+    };
+    
+    
+    public int rule(int a,int b);
+}
+public class Functionalinterface_2
+{
+    public static void main(String[] arg )
+    {
+        a.main(12,12);
+        
+        a o = (a,b)-> a+b;
+       System.out.println( o.rule(24,24));
+    }
+}
