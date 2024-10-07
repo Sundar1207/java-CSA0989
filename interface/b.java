150cffc9c67ec2f7ca69bae4216e9588673901f6
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2024-10-07 23:01:38 +0530

    Add files via upload
    it is static and private interfase

---
+++ interface/b.java
@@ -0,0 +1,21 @@
+interface a 
+{
+    public static void main(int a , int b)
+    {
+        System.out.println("Sum of two numbers : "+(a+b));
+        rule2();
+    }
+    public void rule();
+    private static void rule2()
+    {
+        System.out.println("Hello java ");
+    };
+}
+public class b
+{
+    public static void main(String[] arg )
+    {
+        //b obj=new b();
+        a.main(12,12);
+    }
+}
